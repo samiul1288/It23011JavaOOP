86bcb2ba4c7360c5a989403215ca81af86f62052
Samiul Haque Ruf <165394297+samiul1288@users.noreply.github.com>
2024-11-10 08:16:42 +0600

    Add files via upload
    10 20 30 40 50 60
    Highest Value: 60
    Sum: 210

--- readfileonetoother.java
+++ readfileonetoother.java
@@ -1,6 +1,41 @@
 package javaFileRead;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class readfileonetoother {
+    public static void main(String[] args) {
+        // Define input and output files
+        File inputFile = new File("C:/Users/HP/IdeaProjects/javasamiul/a1.txt");
+        File outputFile = new File("C:/Users/HP/IdeaProjects/javasamiul/a2.txt");
 
+        try (Scanner scanner = new Scanner(inputFile);
+             PrintWriter writer = new PrintWriter(outputFile)) {
 
+            int highest = Integer.MIN_VALUE;
+            int sum = 0;
+
+            // Read integers from the input file
+            while (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                sum += number;
+                if (number > highest) {
+                    highest = number;
+                }
+            }
+
+            // Write the highest value and sum to the output file
+            writer.println("Highest Value: " + highest);
+            writer.println("Sum: " + sum);
+
+            System.out.println("Results written to " + outputFile.getName());
+
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: Input or output file not found.");
+        }
+    }
 }
+
+
+
